48837d2d6f801152c72f3229c662f8de1f1ade43
shanghanrun <idim7@naver.com>
2026-02-15 21:15:12 +0900

    wrangler.jsonc 작성

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  compilerOptions: {
+    // 경고 때문에 로그가 너무 지저분하면 아래 줄을 추가해서 무시하세요.
+    warningFilter: (warning) => !warning.code.startsWith('a11y-')
+  },
   preprocess: vitePreprocess(),
   kit: {
     // adapter-auto 대신 설치한 cloudflare 어댑터를 연결합니다.
